a97f5dece3cbd59f6a9402ea6f832403e9f2bab5
Gaudriol <romain.peter3@gmail.com>
2020-09-13 23:42:19 +0200

    Set up styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { GlobalStyles } from "./styles";
 
 function App() {
-  return <div>Coucou</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>Coucou</div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+  background: "#EAEAEA",
+  backgroundSecondary: "#F8F8F8",
+  text: "#848886",
+  textSecondary: "#aaaaaa",
+  border: "#EAEAEA",
+};
