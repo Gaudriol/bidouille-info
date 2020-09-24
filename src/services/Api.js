4b6446bb8e4fe45cc7fb104d71a7d3da64e196ef
Gaudriol <romain.peter3@gmail.com>
2020-09-24 22:53:52 +0200

    fix api

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-// Default API will be your root
 const API_ROOT = process.env.URL || "http://localhost:3000/";
 const TIMEOUT = 20000;
 const HEADERS = {
-  // "Content-Type": "application/json",
-  // Accept: "application/json",
-  "Access-Control-Allow-Origin": "*",
+  "Content-Type": "application/json",
+  Accept: "application/json",
 };
 
 class ApiService {
